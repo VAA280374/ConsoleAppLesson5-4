606e89dd6ddddb32884779f7efc9a2fe54195d6c
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-02 07:37:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLesson5-4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+            int vCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-50, 50);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine(" ");
+            for (int i = 1; i < array.Length; i = i + 2) 
+            {
+                if (array[i] >= 0)
+                {
+                    int vt = array[i];
+                    // Console.Write(" {0} {1} ", i+1, vt);
+                    vt = vt / 2;
+                    if ((2 * vt) != array[i])
+                    {
+                        vCount++;
+                    }
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Количество нечетных положительных чисел стоящих на четных местах составляет : {0}", vCount);
+            Console.ReadKey();
+        }
+    }
+}
